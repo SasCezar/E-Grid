d7e85768cd641f7b9d6d749516fcd0a29288e4d5
Cezar Sas <SasCezar@users.noreply.github.com>
2020-04-24 17:00:25 +0200

    Update views.py
    Cleaned file

--- egrid/views.py
+++ egrid/views.py
@@ -6,9 +6,8 @@ from egrid.models import Product, Order
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    print(products)
     context = {"products": products}
     return render(request, 'index.html', context)
-    # return HttpResponse("Hello, world. You're at the polls index.")
+
 
 
