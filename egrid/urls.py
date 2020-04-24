8224b4b4fe9d7988c360fc2f2221595414d30f29
MicheleCarbonera <49145321+MicheleCarbonera@users.noreply.github.com>
2020-04-24 20:48:27 +0200

    order basic

--- egrid/urls.py
+++ egrid/urls.py
@@ -4,6 +4,7 @@ from egrid import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about_us.html', views.about_us, name='about_us')
+    path('about_us.html', views.about_us, name='about_us'),
+    path('order.html', views.order, name='order')
 
 ]
