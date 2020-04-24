451a6ab3e6201b36856506e18a0158526244ba0e
Dag <d.tegegn@campus.unimib.it>
2020-04-24 16:02:01 +0200

    Created landing page.

--- egrid/views.py
+++ egrid/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from egrid.models import Product, Order
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    products = Product.objects.all()
+    print(products)
+    context = {"products": products}
+    return render(request, 'index.html', context)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# def request_detail(request, pk):
+#     detail = Order.objects.get(pk=pk)
+#     context = {"detail": detail}
+#     return render(request, "detail.html", context)
