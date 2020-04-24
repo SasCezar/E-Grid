27ece3971b52973b88ce922b12dd30de92bc103d
Cezar Sas <cezar.sas@gmail.com>
2020-04-24 19:51:08 +0200

    Added about_us page

--- egrid/urls.py
+++ egrid/urls.py
@@ -5,6 +5,5 @@ from egrid import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about_us.html', views.about_us, name='about_us')
-    path('order.html', views.order, name='order')
 
 ]
